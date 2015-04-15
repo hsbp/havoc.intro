85e796e09c01850a2f9fc3557569ecf982d0a052
András Veres-Szentkirályi <vsza@vsza.hu>
2015-04-15 10:41:49 +0200

    build.sh: use yuv420p output (default resulted in yuv444)

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 	python logo/gen.py
 	) | ffmpeg -r $(json fps <params.json) -loglevel quiet \
 		-s $(json width <params.json | tr '\n' x ; json height <params.json) \
-		-f rawvideo -pixel_format rgb24 -i - -an -vcodec libx264 -y /tmp/concat.mkv
+		-f rawvideo -pixel_format rgb24 -i - -an -vcodec libx264 -pix_fmt yuv420p -y /tmp/concat.mkv
